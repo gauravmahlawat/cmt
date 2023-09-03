2f2871e214f629a45c5db36db099ca06678989f8
Gaurav Mahlawat <gauravmahlawat@Gauravs-MacBook-Pro.local>
2023-09-03 15:11:24 +0530

    master: Ckeditor added as a text editor

--- static/ceditor.js
+++ static/ceditor.js
@@ -1,7 +1,4 @@
-// This sample still does not showcase all CKEditor&nbsp;5 features (!)
-// Visit https://ckeditor.com/docs/ckeditor5/latest/features/index.html to browse all the features.
-CKEDITOR.ClassicEditor.create(document.getElementById("editor"), {
-    // https://ckeditor.com/docs/ckeditor5/latest/features/toolbar/toolbar.html#extended-toolbar-configuration-format
+CMTEDITOR.ClassicEditor.create(document.getElementById("cmteditor"), {
     toolbar: {
         items: [
             'exportPDF', 'exportWord', '|',
@@ -21,8 +18,6 @@ CKEDITOR.ClassicEditor.create(document.getElementById("editor"), {
         ],
         shouldNotGroupWhenFull: true
     },
-    // Changing the language of the interface requires loading the language file using the <script> tag.
-    // language: 'es',
     list: {
         properties: {
             styles: true,
@@ -30,21 +25,18 @@ CKEDITOR.ClassicEditor.create(document.getElementById("editor"), {
             reversed: true
         }
     },
-    // https://ckeditor.com/docs/ckeditor5/latest/features/headings.html#configuration
     heading: {
         options: [
-            { model: 'paragraph', title: 'Paragraph', class: 'ck-heading_paragraph' },
-            { model: 'heading1', view: 'h1', title: 'Heading 1', class: 'ck-heading_heading1' },
-            { model: 'heading2', view: 'h2', title: 'Heading 2', class: 'ck-heading_heading2' },
-            { model: 'heading3', view: 'h3', title: 'Heading 3', class: 'ck-heading_heading3' },
-            { model: 'heading4', view: 'h4', title: 'Heading 4', class: 'ck-heading_heading4' },
-            { model: 'heading5', view: 'h5', title: 'Heading 5', class: 'ck-heading_heading5' },
-            { model: 'heading6', view: 'h6', title: 'Heading 6', class: 'ck-heading_heading6' }
+            { model: 'paragraph', title: 'Paragraph', class: 'cmt-heading_paragraph' },
+            { model: 'heading1', view: 'h1', title: 'Heading 1', class: 'cmt-heading_heading1' },
+            { model: 'heading2', view: 'h2', title: 'Heading 2', class: 'cmt-heading_heading2' },
+            { model: 'heading3', view: 'h3', title: 'Heading 3', class: 'cmt-heading_heading3' },
+            { model: 'heading4', view: 'h4', title: 'Heading 4', class: 'cmt-heading_heading4' },
+            { model: 'heading5', view: 'h5', title: 'Heading 5', class: 'cmt-heading_heading5' },
+            { model: 'heading6', view: 'h6', title: 'Heading 6', class: 'cmt-heading_heading6' }
         ]
     },
-    // https://ckeditor.com/docs/ckeditor5/latest/features/editor-placeholder.html#using-the-editor-configuration
-    placeholder: 'Welcome to CKEditor&nbsp;5!',
-    // https://ckeditor.com/docs/ckeditor5/latest/features/font.html#configuring-the-font-family-feature
+    placeholder: 'Welcome to CMTEDITOR!',
     fontFamily: {
         options: [
             'default',
@@ -59,13 +51,10 @@ CKEDITOR.ClassicEditor.create(document.getElementById("editor"), {
         ],
         supportAllValues: true
     },
-    // https://ckeditor.com/docs/ckeditor5/latest/features/font.html#configuring-the-font-size-feature
     fontSize: {
         options: [10, 12, 14, 'default', 18, 20, 22],
         supportAllValues: true
     },
-    // Be careful with the setting below. It instructs CKEditor to accept ALL HTML markup.
-    // https://ckeditor.com/docs/ckeditor5/latest/features/general-html-support.html#enabling-all-html-features
     htmlSupport: {
         allow: [
             {
@@ -76,12 +65,9 @@ CKEDITOR.ClassicEditor.create(document.getElementById("editor"), {
             }
         ]
     },
-    // Be careful with enabling previews
-    // https://ckeditor.com/docs/ckeditor5/latest/features/html-embed.html#content-previews
     htmlEmbed: {
         showPreviews: true
     },
-    // https://ckeditor.com/docs/ckeditor5/latest/features/link.html#custom-link-attributes-decorators
     link: {
         decorators: {
             addTargetToExternalLinks: true,
@@ -95,7 +81,6 @@ CKEDITOR.ClassicEditor.create(document.getElementById("editor"), {
             }
         }
     },
-    // https://ckeditor.com/docs/ckeditor5/latest/features/mentions.html#configuration
     mention: {
         feeds: [
             {
@@ -110,21 +95,10 @@ CKEDITOR.ClassicEditor.create(document.getElementById("editor"), {
             }
         ]
     },
-    // The "super-build" contains more premium features that require additional configuration, disable them below.
-    // Do not turn them on unless you read the documentation and know how to configure them and setup the editor.
     removePlugins: [
-        // These two are commercial, but you can try them out without registering to a trial.
-        // 'ExportPdf',
-        // 'ExportWord',
         'CKBox',
         'CKFinder',
         'EasyImage',
-        // This sample uses the Base64UploadAdapter to handle image uploads as it requires no configuration.
-        // https://ckeditor.com/docs/ckeditor5/latest/features/images/image-upload/base64-upload-adapter.html
-        // Storing images as Base64 is usually a very bad idea.
-        // Replace it on production website with other solutions:
-        // https://ckeditor.com/docs/ckeditor5/latest/features/images/image-upload/image-upload.html
-        // 'Base64UploadAdapter',
         'RealTimeCollaborativeComments',
         'RealTimeCollaborativeTrackChanges',
         'RealTimeCollaborativeRevisionHistory',
@@ -135,10 +109,7 @@ CKEDITOR.ClassicEditor.create(document.getElementById("editor"), {
         'RevisionHistory',
         'Pagination',
         'WProofreader',
-        // Careful, with the Mathtype plugin CKEditor will not load when loading this sample
-        // from a local file system (file://) - load this site via HTTP server if you enable MathType.
         'MathType',
-        // The following features are part of the Productivity Pack and require additional license.
         'SlashCommand',
         'Template',
         'DocumentOutline',
